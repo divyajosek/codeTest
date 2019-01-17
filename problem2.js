32fbd4fa807471a71ff1f390a8b2ab5269529056
divyajosek <divya.jose@microsemi.com>
2019-01-17 13:44:19 -0800

    Update problem2.js

--- problem2.js
+++ problem2.js
@@ -3,11 +3,14 @@
  Find the sum of all the multiples of 3 or 5 below 1000.
 
 ********************************************************/
-
+//Initialising sum
 let sum =0;
+let limit=1000;
 
-for(let i=0;i<1000;i++){
+//Loop till the limit
+for(let i=0;i<limit;i++){
   
+ //Check for multiples of 3 & 5
   if((i%3 == 0) || (i%5 ==0 )){
        sum +=i;
     
